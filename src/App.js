e1dc6e64f62b9ec4dfe06ced67110f46b2e702f2
Dmitriy Dudnik <wed2288@gmail.com>
2022-01-11 23:18:57 +0200

    добавляет начальную разметку

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Please leave feedback</h2>
+      <ul>
+        <li>
+          <button>Good</button>
+        </li>
+        <li>
+          <button>Neutral</button>
+        </li>
+        <li>
+          <button>Bad</button>
+        </li>
+      </ul>
+      <div>
+        <h3>Statistics</h3>
+        <ul>
+          <li>
+            <p>Good:</p>
+          </li>
+          <li>
+            <p>Neutral:</p>
+          </li>
+          <li>
+            <p>Bad:</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
