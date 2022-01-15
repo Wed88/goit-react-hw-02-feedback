cb7c39ad95c5ff07be006b18e06948ef129d086e
Dmitriy Dudnik <wed2288@gmail.com>
2022-01-15 20:42:44 +0200

    выполняет 4 шаг

--- src/components/Statistics/Statistics .jsx
+++ src/components/Statistics/Statistics .jsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import Notification from './Notification/Notification';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
   <div>
     <h2>Statistics</h2>
-    <ul>
-      <li>
-        <p>Good: {good}</p>
-      </li>
-      <li>
-        <p>Neutral: {neutral}</p>
-      </li>
-      <li>
-        <p>Bad: {bad}</p>
-      </li>
-      <li>
-        <p>Total: {total}</p>
-      </li>
-      <li>
-        <p>Positive feedback: {positivePercentage}%</p>
-      </li>
-    </ul>
+    {total > 0 ? (
+      <ul>
+        <li>
+          <p>Good: {good}</p>
+        </li>
+        <li>
+          <p>Neutral: {neutral}</p>
+        </li>
+        <li>
+          <p>Bad: {bad}</p>
+        </li>
+        <li>
+          <p>Total: {total}</p>
+        </li>
+        <li>
+          <p>Positive feedback: {positivePercentage}%</p>
+        </li>
+      </ul>
+    ) : (
+      <Notification message="There is no feedback" />
+    )}
   </div>
 );
 
