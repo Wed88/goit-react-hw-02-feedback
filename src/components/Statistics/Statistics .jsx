da030a4da05d789ca9571a22e329ee256778056f
Dmitriy Dudnik <wed2288@gmail.com>
2022-01-15 20:24:56 +0200

    выполняет 3 шаг

---
+++ src/components/Statistics/Statistics .jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div>
+    <h2>Statistics</h2>
+    <ul>
+      <li>
+        <p>Good: {good}</p>
+      </li>
+      <li>
+        <p>Neutral: {neutral}</p>
+      </li>
+      <li>
+        <p>Bad: {bad}</p>
+      </li>
+      <li>
+        <p>Total: {total}</p>
+      </li>
+      <li>
+        <p>Positive feedback: {positivePercentage}%</p>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Statistics;
